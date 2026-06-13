acc06ab17c94a1476f5010945fe77b9ac59b16ba
Xavios <mr.acs.andras@gmail.com>
2026-06-13 09:10:00 +0200

    fix up the squares of two largest numbers out of three

--- journey/001_recursion/recursion.scm
+++ journey/001_recursion/recursion.scm
@@ -14,8 +14,12 @@
 ; as arguments and returns the sum of the squares of the two
 ; larger numbers.
 
+
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+
 (define (sum-of-two-largest a b c)
   (cond
-   ((and (> a c) (> b c)) (+ a b))
-   ((and (> b a) (> c a)) (+ b c))
-   ((and (> a b) (> c b)) (+ a c))))
+   ((and (> a c) (> b c)) (sum-of-squares a b))
+   ((and (> b a) (> c a)) (sum-of-squares b c))
+   ((and (> a b) (> c b)) (sum-of-squares a c))))
